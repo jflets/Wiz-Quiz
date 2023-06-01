044c9a52e38d73b905e07cf72a3d2cc90b371b22
jflets <jfletorides02@gmail.com>
2023-06-01 13:43:51 +0100

    Add initial HTML questions to script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,47 @@ function closeModal(modal) {
     modal.classList.remove('active')
     overlay.classList.remove('active')
 }
+
+//question area
+const quizData = [
+    {
+        question: "What does HTML stand for?",
+        a: "Hyper Text Markup Language",
+        b: "High Tech Machine Learning",
+        c: "Home Tool Management Language",
+        d: "Human Task Modeling Logic",
+        correct: "a",
+    },
+    {
+        question: "Which tag is used to define the main content of an HTML document?",
+        a: "<header>",
+        b: "<footer>",
+        c: "<main>",
+        d: "<section>",
+        correct: "c", 
+    },
+    {
+        question: "What is the correct way to create a hyperlink in HTML?",
+        a: "<link>",
+        b: "<a href=''>",
+        c: "<url>",
+        d: "<hyperlink>",
+        correct: "b",
+    },
+    {
+        question: "Which attribute is used to specify the CSS styling for an HTML element?",
+        a: "style",
+        b: "class",
+        c: "id",
+        d: "src",
+        correct: "a",
+    },
+    {
+        question: "What is the correct HTML tag for inserting a line break?",
+        a: "<br>",
+        b: "<break>",
+        c: "<lb>",
+        d: "<newline>",
+        correct: "a",
+    },
+];
