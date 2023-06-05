bcd8f089ca4001ddfdd9dbcbd34af08006493b6b
jflets <jfletorides02@gmail.com>
2023-06-05 20:12:50 +0100

    Add comments and format code on all pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,189 +1,197 @@
 //modal code
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
 
-openModalButtons.forEach(button => {
-    button.addEventListener('click' , () => {
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
-})
+openModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
 
-closeModalButtons.forEach(button => {
-    button.addEventListener('click' , () => {
-        const modal = button.closest('.modal')
-        closeModal(modal)
-    })
-})
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
 
 function openModal(modal) {
-    if (modal == null) return;
-    modal.classList.add('active')
-    overlay.classList.add('active')
+  if (modal == null) return;
+  modal.classList.add("active");
+  overlay.classList.add("active");
 }
 
 function closeModal(modal) {
-    if (modal == null) return;
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
+  if (modal == null) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
 }
 
 //question area
 const quizDataHTML = [
-    {
-      question: "What does HTML stand for?",
-      a: "Hyper Text Markup Language",
-      b: "High Tech Machine Learning",
-      c: "Home Tool Management Language",
-      d: "Human Task Modeling Logic",
-      correct: "a",
-    },
-    {
-      question: "Which tag is used to define the main content of an HTML document?",
-      a: "<header>",
-      b: "<footer>",
-      c: "<main>",
-      d: "<section>",
-      correct: "c",
-    },
-    {
-      question: "What is the correct way to create a hyperlink in HTML?",
-      a: "<link>",
-      b: "<a href=''>",
-      c: "<url>",
-      d: "<hyperlink>",
-      correct: "b",
-    },
-    {
-      question: "Which attribute is used to specify the CSS styling for an HTML element?",
-      a: "style",
-      b: "class",
-      c: "id",
-      d: "src",
-      correct: "a",
-    },
-    {
-      question: "What is the correct HTML tag for inserting a line break?",
-      a: "<br>",
-      b: "<break>",
-      c: "<lb>",
-      d: "<newline>",
-      correct: "a",
-    },
-  ];
-  
-  const quizDataJavaScript = [
-    {
-      question: "What is JavaScript?",
-      a: "A programming language",
-      b: "A style sheet language",
-      c: "A markup language",
-      d: "A database query language",
-      correct: "a",
-    },
-    {
-      question: "Which statement is used to declare a JavaScript variable?",
-      a: "let",
-      b: "var",
-      c: "const",
-      d: "All of the above",
-      correct: "d",
-    },
-    {
-      question: "What is the output of the following code? console.log(2 + 2 + '2');",
-      a: "4",
-      b: "22",
-      c: "24",
-      d: "None of the above",
-      correct: "b",
-    },
-    {
-      question: "Which method is used to add an element to the end of an array in JavaScript?",
-      a: "push()",
-      b: "pop()",
-      c: "shift()",
-      d: "unshift()",
-      correct: "a",
-    },
-    {
-      question: "What is the result of the following expression? typeof null;",
-      a: "'object'",
-      b: "'null'",
-      c: "'undefined'",
-      d: "'number'",
-      correct: "a",
-    },
-  ];
-  
-  const quiz = document.getElementById('game-page-wrapper');
-  const answerEls = document.querySelectorAll('.answer-buttons input');
-  const questionEls = document.getElementById('question');
-  const a_text = document.getElementById('a-btn');
-  const b_text = document.getElementById('b-btn');
-  const c_text = document.getElementById('c-btn');
-  const d_text = document.getElementById('d-btn');
-  const submitBtn = document.getElementById('submit');
-  
-  let currentQuiz = 0;
-  let score = 0;
-  let currentQuizData = quizDataHTML;
-  
-  loadQuiz();
-  
-  function loadQuiz() {
-    deselectAnswer();
-    currentQuizData = getCurrentQuizData();
-    questionEls.innerText = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
-    d_text.innerText = currentQuizData.d;
-  }
-  
-  function getCurrentQuizData() {
-    return currentQuiz < quizDataHTML.length ? quizDataHTML[currentQuiz] : quizDataJavaScript[currentQuiz - quizDataHTML.length];
-  }
-  
-  function deselectAnswer() {
-    answerEls.forEach(answerEl => {
-      answerEl.checked = false;
-    });
-  }
-  
-  function getSelected() {
-    let answer;
-    answerEls.forEach(answerEl => {
-      if (answerEl.checked) {
-        answer = answerEl.id;
-      }
-    });
-    return answer;
-  }
-  
-  const selectedQuiz = sessionStorage.getItem('selectedQuiz');
-  if (selectedQuiz === 'javascript') {
-    currentQuizData = quizDataJavaScript;
-  }
+  {
+    question: "What does HTML stand for?",
+    a: "Hyper Text Markup Language",
+    b: "High Tech Machine Learning",
+    c: "Home Tool Management Language",
+    d: "Human Task Modeling Logic",
+    correct: "a",
+  },
+  {
+    question:
+      "Which tag is used to define the main content of an HTML document?",
+    a: "<header>",
+    b: "<footer>",
+    c: "<main>",
+    d: "<section>",
+    correct: "c",
+  },
+  {
+    question: "What is the correct way to create a hyperlink in HTML?",
+    a: "<link>",
+    b: "<a href=''>",
+    c: "<url>",
+    d: "<hyperlink>",
+    correct: "b",
+  },
+  {
+    question:
+      "Which attribute is used to specify the CSS styling for an HTML element?",
+    a: "style",
+    b: "class",
+    c: "id",
+    d: "src",
+    correct: "a",
+  },
+  {
+    question: "What is the correct HTML tag for inserting a line break?",
+    a: "<br>",
+    b: "<break>",
+    c: "<lb>",
+    d: "<newline>",
+    correct: "a",
+  },
+];
 
-  const buttonName = selectedQuiz === 'javascript' ? 'JavaScript Quiz' : 'HTML Quiz';
-  const titleEl = document.getElementById('quiz-title');
-  titleEl.innerText = buttonName;
-  
-  submitBtn.addEventListener('click', () => {
-    const answer = getSelected();
-    if (answer) {
-      if (answer === currentQuizData.correct) {
-        score++;
-      }
-  
-      currentQuiz++;
-  
-      if (currentQuiz < quizDataHTML.length + quizDataJavaScript.length) {
-        loadQuiz();
-      } else {
-        quiz.innerHTML = `<h4>You answered ${score}/${quizDataHTML.length + quizDataJavaScript.length} questions correctly</h4><button id="reload" onclick="location.reload()">Reload</button>`;
-      }
+const quizDataJavaScript = [
+  {
+    question: "What is JavaScript?",
+    a: "A programming language",
+    b: "A style sheet language",
+    c: "A markup language",
+    d: "A database query language",
+    correct: "a",
+  },
+  {
+    question: "Which statement is used to declare a JavaScript variable?",
+    a: "let",
+    b: "var",
+    c: "const",
+    d: "All of the above",
+    correct: "d",
+  },
+  {
+    question:
+      "What is the output of the following code? console.log(2 + 2 + '2');",
+    a: "4",
+    b: "22",
+    c: "24",
+    d: "None of the above",
+    correct: "b",
+  },
+  {
+    question:
+      "Which method is used to add an element to the end of an array in JavaScript?",
+    a: "push()",
+    b: "pop()",
+    c: "shift()",
+    d: "unshift()",
+    correct: "a",
+  },
+  {
+    question: "What is the result of the following expression? typeof null;",
+    a: "'object'",
+    b: "'null'",
+    c: "'undefined'",
+    d: "'number'",
+    correct: "a",
+  },
+];
+
+const quiz = document.getElementById("game-page-wrapper");
+const answerEls = document.querySelectorAll(".answer-buttons input");
+const questionEls = document.getElementById("question");
+const a_text = document.getElementById("a-btn");
+const b_text = document.getElementById("b-btn");
+const c_text = document.getElementById("c-btn");
+const d_text = document.getElementById("d-btn");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+let currentQuizData = quizDataHTML;
+
+loadQuiz();
+
+function loadQuiz() {
+  deselectAnswer();
+  currentQuizData = getCurrentQuizData();
+  questionEls.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+function getCurrentQuizData() {
+  return currentQuiz < quizDataHTML.length
+    ? quizDataHTML[currentQuiz]
+    : quizDataJavaScript[currentQuiz - quizDataHTML.length];
+}
+
+function deselectAnswer() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
+}
+
+function getSelected() {
+  let answer;
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
     }
   });
-  
+  return answer;
+}
+// User choice title
+const selectedQuiz = sessionStorage.getItem("selectedQuiz");
+if (selectedQuiz === "javascript") {
+  currentQuizData = quizDataJavaScript;
+}
+
+const buttonName =
+  selectedQuiz === "javascript" ? "JavaScript Quiz" : "HTML Quiz";
+const titleEl = document.getElementById("quiz-title");
+titleEl.innerText = buttonName;
+
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+  if (answer) {
+    if (answer === currentQuizData.correct) {
+      score++;
+    }
+
+    currentQuiz++;
+
+    if (currentQuiz < quizDataHTML.length + quizDataJavaScript.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `<h4>You answered ${score}/${
+        quizDataHTML.length + quizDataJavaScript.length
+      } questions correctly</h4><button id="reload" onclick="location.reload()">Reload</button>`;
+    }
+  }
+});
