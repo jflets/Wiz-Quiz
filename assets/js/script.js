478fe0097840c05d82e30f1b5a63a341a3ba0fae
jflets <jfletorides02@gmail.com>
2023-06-05 17:14:53 +0100

    Change styling of game page

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,97 +31,101 @@ function closeModal(modal) {
 
 //question area
 const quizData = [
-    {
-        question: "What does HTML stand for?",
-        a: "Hyper Text Markup Language",
-        b: "High Tech Machine Learning",
-        c: "Home Tool Management Language",
-        d: "Human Task Modeling Logic",
-        correct: "a",
-    },
-    {
-        question: "Which tag is used to define the main content of an HTML document?",
-        a: "<header>",
-        b: "<footer>",
-        c: "<main>",
-        d: "<section>",
-        correct: "c", 
-    },
-    {
-        question: "What is the correct way to create a hyperlink in HTML?",
-        a: "<link>",
-        b: "<a href=''>",
-        c: "<url>",
-        d: "<hyperlink>",
-        correct: "b",
-    },
-    {
-        question: "Which attribute is used to specify the CSS styling for an HTML element?",
-        a: "style",
-        b: "class",
-        c: "id",
-        d: "src",
-        correct: "a",
-    },
-    {
-        question: "What is the correct HTML tag for inserting a line break?",
-        a: "<br>",
-        b: "<break>",
-        c: "<lb>",
-        d: "<newline>",
-        correct: "a",
-    },
+  {
+    question: "What does HTML stand for?",
+    a: "Hyper Text Markup Language",
+    b: "High Tech Machine Learning",
+    c: "Home Tool Management Language",
+    d: "Human Task Modeling Logic",
+    correct: "a",
+  },
+  {
+    question: "Which tag is used to define the main content of an HTML document?",
+    a: "<header>",
+    b: "<footer>",
+    c: "<main>",
+    d: "<section>",
+    correct: "c",
+  },
+  {
+    question: "What is the correct way to create a hyperlink in HTML?",
+    a: "<link>",
+    b: "<a href=''>",
+    c: "<url>",
+    d: "<hyperlink>",
+    correct: "b",
+  },
+  {
+    question: "Which attribute is used to specify the CSS styling for an HTML element?",
+    a: "style",
+    b: "class",
+    c: "id",
+    d: "src",
+    correct: "a",
+  },
+  {
+    question: "What is the correct HTML tag for inserting a line break?",
+    a: "<br>",
+    b: "<break>",
+    c: "<lb>",
+    d: "<newline>",
+    correct: "a",
+  },
 ];
 
 const quiz = document.getElementById('game-page-wrapper');
-const answerEls = document.querySelectorAll('.answer-buttons');
+const answerEls = document.querySelectorAll('.answer-buttons input');
 const questionEls = document.getElementById('question');
 const a_text = document.getElementById('a-btn');
 const b_text = document.getElementById('b-btn');
 const c_text = document.getElementById('c-btn');
 const d_text = document.getElementById('d-btn');
-const submitBtn = document.getElementById('submit-btn')
+const submitBtn = document.getElementById('submit');
 
 let currentQuiz = 0;
 let score = 0;
 
-loadQuiz()
+loadQuiz();
+
 function loadQuiz() {
-    deselectAnswer();
-    const currentQuizData = quizData[currentQuiz];
-    questionEls.innerText = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
-    d_text.innerText = currentQuizData.d;
+  deselectAnswer();
+  const currentQuizData = quizData[currentQuiz];
+  questionEls.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
 }
- function deselectAnswer() {
-    answerEls.forEach(answerEl => answerEl.checked = false)
- }
 
- function getSelected() {
-    let answer
-    answerEls.forEach(answerEls => {
-        if(answerEls.checked) {
-            answer = answerEls.id
-        }
-    })
-    return answer
- }
+function deselectAnswer() {
+  answerEls.forEach(answerEl => {
+    answerEl.checked = false;
+  });
+}
 
- submitBtn.addEventListener('click', () => {
-    const answer = getSelected()
-    if(answer) {
-        if (answer === quizData[currentQuiz].correct) {
-            score++
-        }
-
-        currentQuiz++
-
-        if(currentQuiz < quizData.length) {
-            loadQuiz()
-        } else {
-            quiz.innerHTML = '<h4>You answered ${score}/${quizData.length} questions correctly</h4> <button onclick="location.reload()">Reaload</button>'
-        }
+function getSelected() {
+  let answer;
+  answerEls.forEach(answerEl => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
     }
- })
+  });
+  return answer;
+}
+
+submitBtn.addEventListener('click', () => {
+  const answer = getSelected();
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+
+    currentQuiz++;
+
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `<h4>You answered ${score}/${quizData.length} questions correctly</h4><button id="reload" onclick="location.reload()">Reload</button>`;
+    }
+  }
+});
